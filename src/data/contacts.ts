d13c321f13676bdbe68a96095f334ff26f6d9b82
Lucy Sanders <lucyhsanders@gmail.com>
2021-04-20 22:19:00 -0600

    changed to /images

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -9,19 +9,19 @@ export const contacts: {[index: string]: IContact} = {
   "github": {
     url:"https://github.com/lulusanders84",
     title:"Github",
-    imageSrc:"%PUBLIC_URL%/images/w-github-logo.png",
+    imageSrc:"/images/w-github-logo.png",
     alt:"Github Link"
   },
   "gmail": {
     url: "mailto:lucyhsanders@gmail.com", 
     title:"Gmail", 
-    imageSrc:"%PUBLIC_URL%/images/gmail-logo.png", 
+    imageSrc:"/images/gmail-logo.png", 
     alt:"Gmail Link"
   },
   "linkedIn": {
     url: "https://www.linkedin.com/in/lucy-sanders-b1333799/",
     title:"LinkedIn", 
-    imageSrc:"%PUBLIC_URL%/images/linkedin-logo.png", 
+    imageSrc:"/images/linkedin-logo.png", 
     alt:"LinkedIn Link" 
   }
 }
